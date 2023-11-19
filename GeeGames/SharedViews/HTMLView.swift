5142495923bbc4b272ffd9459b016056f3dd5ddd
Roman Bigun <proffcoders@gmail.com>
2023-11-19 17:03:48 +0200

    - force dark mode

---
+++ GeeGames/SharedViews/HTMLView.swift
@@ -0,0 +1,18 @@
+//
+//  HTMLView.swift
+//  GeeGames
+//
+//  Created by Roman Bigun on 19.11.2023.
+//
+
+import SwiftUI
+
+struct HTMLView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HTMLView()
+}
