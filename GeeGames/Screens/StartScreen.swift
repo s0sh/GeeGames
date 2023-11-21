47ffb637c3e4c64cd14712fd74620da713e4dfc1
Roman Bigun <proffcoders@gmail.com>
2023-11-21 14:42:39 +0200

    - Start Screen

---
+++ GeeGames/Screens/StartScreen.swift
@@ -0,0 +1,18 @@
+//
+//  StartScreen.swift
+//  GeeGames
+//
+//  Created by Roman Bigun on 21.11.2023.
+//
+
+import SwiftUI
+
+struct StartScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartScreen()
+}
---
+++ GeeGames/Store.swift
@@ -0,0 +1,8 @@
+//
+//  Store.swift
+//  GeeGames
+//
+//  Created by Roman Bigun on 20.11.2023.
+//
+
+import Foundation
